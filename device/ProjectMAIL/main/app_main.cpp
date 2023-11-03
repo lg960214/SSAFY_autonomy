24c9d5b9811767f5f2eada1dd7293a35fb4ac314
Byeongmin Ryoo <bmryu0501>
2023-11-03 16:28:09 +0900

    [S09P31A305-329] :sparkles: : Add LoadModelFromSPIFFS

--- device/ProjectMAIL/main/app_main.cpp
+++ device/ProjectMAIL/main/app_main.cpp
@@ -1,8 +1,13 @@
 #include <esp_log.h>
+//#include <esp_err.h>
+
+#include "ondevice_ai.h"
 
 static const char* TAG = "app_main";
 
 extern "C" void app_main(void)
 {
     ESP_LOGI(TAG, "Hello");
+    
+    printTest();
 }
