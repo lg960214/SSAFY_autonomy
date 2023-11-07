012af9f334eabc8ba6b3bb60c0ba5d99e8b57d14
euiseon hong <hon3538@gmail.com>
2023-11-07 17:57:50 +0900

    :sparkles: : Add device features

---
+++ device/ProjectMAIL/main/app_driver.cpp
@@ -0,0 +1,92 @@
+/*
+   This example code is in the Public Domain (or CC0 licensed, at your option.)
+
+   Unless required by applicable law or agreed to in writing, this
+   software is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
+   CONDITIONS OF ANY KIND, either express or implied.
+*/
+
+#include <esp_log.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include <device.h>
+#include <esp_matter.h>
+#include <led_driver.h>
+
+#include <app_priv.h>
+
+using namespace chip::app::Clusters;
+using namespace esp_matter;
+using namespace esp_matter::cluster;
+static const char *TAG = "app_driver";
+extern uint16_t light_endpoint_id;
+
+/* Do any conversions/remapping for the actual value here */
+static esp_err_t app_driver_light_set_power(led_driver_handle_t handle, esp_matter_attr_val_t *val)
+{
+    return led_driver_set_power(handle, val->val.b);
+}
+
+static esp_err_t app_driver_light_set_brightness(led_driver_handle_t handle, esp_matter_attr_val_t *val)
+{
+    int value = REMAP_TO_RANGE(val->val.u8, MATTER_BRIGHTNESS, STANDARD_BRIGHTNESS);
+    return led_driver_set_brightness(handle, value);
+}
+
+esp_err_t app_driver_attribute_update(app_driver_handle_t driver_handle, uint16_t endpoint_id, uint32_t cluster_id,
+                                      uint32_t attribute_id, esp_matter_attr_val_t *val)
+{
+    // ESP_LOGW(TAG, "App driver attribute update!! endpoint : %u, cluster : %lu, attribute : %lu ", endpoint_id, cluster_id, attribute_id);
+    esp_err_t err = ESP_OK;
+    if (endpoint_id == light_endpoint_id) {
+        // ESP_LOGW(TAG, "Light endpoint attribute update!! endpoint : %u, cluster : %lu, attribute : %lu ", endpoint_id, cluster_id, attribute_id);
+        led_driver_handle_t handle = (led_driver_handle_t)driver_handle;
+        if (cluster_id == OnOff::Id) {
+            if (attribute_id == OnOff::Attributes::OnOff::Id) {
+                // ESP_LOGW(TAG, "Light OnOff Attribute Changed! ");
+                err = app_driver_light_set_power(handle, val);
+            }
+        } else if (cluster_id == LevelControl::Id) {
+            if (attribute_id == LevelControl::Attributes::CurrentLevel::Id) {
+                // ESP_LOGW(TAG, "LevelControl Attribute Changed! ");
+                err = app_driver_light_set_brightness(handle, val);
+            }
+        }
+    }
+    return err;
+}
+
+esp_err_t app_driver_light_set_defaults(uint16_t endpoint_id)
+{
+    esp_err_t err = ESP_OK;
+    void *priv_data = endpoint::get_priv_data(endpoint_id);
+    led_driver_handle_t handle = (led_driver_handle_t)priv_data;
+    node_t *node = node::get();
+    endpoint_t *endpoint = endpoint::get(node, endpoint_id);
+    cluster_t *cluster = NULL;
+    attribute_t *attribute = NULL;
+    esp_matter_attr_val_t val = esp_matter_invalid(NULL);
+
+    /* Setting brightness */
+    cluster = cluster::get(endpoint, LevelControl::Id);
+    attribute = attribute::get(cluster, LevelControl::Attributes::CurrentLevel::Id);
+    attribute::get_val(attribute, &val);
+    err |= app_driver_light_set_brightness(handle, &val);
+
+    /* Setting power */
+    cluster = cluster::get(endpoint, OnOff::Id);
+    attribute = attribute::get(cluster, OnOff::Attributes::OnOff::Id);
+    attribute::get_val(attribute, &val); // attribute value 를 val 에 copy
+    err |= app_driver_light_set_power(handle, &val);
+    
+    return err;
+}
+
+app_driver_handle_t app_driver_light_init()
+{
+    /* Initialize led */
+    led_driver_config_t config = led_driver_get_config();
+    led_driver_handle_t handle = led_driver_init(&config);
+    return (app_driver_handle_t)handle;
+}
