e836f3be83712bc10d1e13787fe91585f0922aef
enduser0@naver.com <enduser0@naver.com>
2023-11-07 10:42:57 +0900

    [S09P31A305-255] ♻️ : remove HTTPS pam file

--- device/esp_http_client/main/esp_http_client_example.c
+++ device/esp_http_client/main/esp_http_client_example.c
@@ -33,7 +33,7 @@
 #include "esp_spiffs.h"
 
 #define MAX_HTTP_RECV_BUFFER 512
-#define MAX_HTTP_OUTPUT_BUFFER 2048
+#define MAX_HTTP_OUTPUT_BUFFER 3000
 static const char *TAG = "HTTP_CLIENT";
 
 /* Root cert for howsmyssl.com, taken from howsmyssl_com_root_cert.pem
@@ -46,12 +46,6 @@ static const char *TAG = "HTTP_CLIENT";
    To embed it in the app binary, the PEM file is named
    in the component.mk COMPONENT_EMBED_TXTFILES variable.
 */
-extern const char howsmyssl_com_root_cert_pem_start[] asm("_binary_howsmyssl_com_root_cert_pem_start");
-extern const char howsmyssl_com_root_cert_pem_end[]   asm("_binary_howsmyssl_com_root_cert_pem_end");
-
-extern const char postman_root_cert_pem_start[] asm("_binary_postman_root_cert_pem_start");
-extern const char postman_root_cert_pem_end[]   asm("_binary_postman_root_cert_pem_end");
-
 
 esp_err_t _http_event_handler(esp_http_client_event_t *evt)
 {
