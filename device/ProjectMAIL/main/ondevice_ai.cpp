ca8df1a9be9c5a236baac9096c24980e5b1114af
Byeongmin Ryoo <bmryu0501>
2023-11-07 09:30:51 +0900

    [S09P31A305-342] :sparkles: : Add AI feature

--- device/ProjectMAIL/main/ondevice_ai.cpp
+++ device/ProjectMAIL/main/ondevice_ai.cpp
@@ -1,5 +1,7 @@
 #include "ondevice_ai.h"
 
+#include "dirent.h"
+
 const char* TAG = "ondevice_ai";
 
 namespace {
@@ -11,54 +13,118 @@ int inference_count = 0;
 
 constexpr int kTensorArenaSize = 2000;
 uint8_t tensor_arena[kTensorArenaSize];
-}  // namespace
+}
 
-class FlatBufferModel {
-  // Build a model based on a file. Return a nullptr in case of failure.
-  static std::unique_ptr<FlatBufferModel> BuildFromFile(
-      const char* filename,
-      ErrorReporter* error_reporter);
+const float kXrange = 2.f * 3.14159265359f;
+const int kInferencesPerCycle = 20;
 
-  // Build a model based on a pre-loaded flatbuffer. The caller retains
-  // ownership of the buffer and should keep it alive until the returned object
-  // is destroyed. Return a nullptr in case of failure.
-  static std::unique_ptr<FlatBufferModel> BuildFromBuffer(
-      const char* buffer,
-      size_t buffer_size,
-      ErrorReporter* error_reporter);
-};
-
-std::unique_ptr<tflite::FlatBufferModel> loadModelFromSPIFFS(const char* model_path) {
-
+void setModel() {
     // SPIFFS Configuration
     esp_vfs_spiffs_conf_t conf = {
       .base_path = "/spiffs",
       .partition_label = NULL,
-      .max_files = 1,
+      .max_files = 5,
       .format_if_mount_failed = true
     };
 
     // Initialize SPIFFS
+    ESP_LOGI(TAG, "Load SPIFFS");
     esp_err_t ret = esp_vfs_spiffs_register(&conf);
     if (ret != ESP_OK) {
         ESP_LOGE("SPIFFS", "Failed to initialize SPIFFS");
-        return nullptr;
+        return;
+    }
+    
+    ESP_LOGI(TAG, "Open directory");
+    DIR *dir = opendir("/spiffs/");
+
+    ESP_LOGI("SPIFFS", "List of files:");
+    struct dirent *ent;
+    while ((ent = readdir(dir)) != NULL) {
+        ESP_LOGI("SPIFFS", "  %s", ent->d_name);
     }
 
-    // Create the FlatBufferModel object directly from file
-    tflite::StderrReporter error_reporter;
-    auto model = tflite::FlatBufferModel::BuildFromFile(model_path, &error_reporter);
+    // Cleanup
+    closedir(dir);
 
-    if (!model) {
-        ESP_LOGE("TFLite", "Failed to load model");
-        esp_vfs_spiffs_unregister(NULL); // Unregister SPIFFS
-        return nullptr;
+    // Read the .tflite model from SPIFFS
+    FILE* file = fopen("/spiffs/hello_world_int8.tflite", "rb");
+    if (file == NULL) {
+        ESP_LOGE("SPIFFS", "Failed to open model file");
+        return;
     }
 
+    ESP_LOGI(TAG, "Fseek");
+    fseek(file, 0, SEEK_END);
+    size_t modelSize = ftell(file);
+    fseek(file, 0, SEEK_SET);
+
+    ESP_LOGI(TAG, "ModelBuffer");
+    uint8_t* modelBuffer = (uint8_t*) malloc(modelSize);
+    fread(modelBuffer, 1, modelSize, file);
+    fclose(file);
+
+    ESP_LOGI(TAG, "Load model");
+    model = tflite::GetModel(modelBuffer);
+
+    ESP_LOGI(TAG, "Unregist model");
     esp_vfs_spiffs_unregister(NULL); // Unregister SPIFFS
-    return model;
+
+    ESP_LOGI(TAG, "Make resolver");
+    static tflite::MicroMutableOpResolver<1> resolver;
+    resolver.AddFullyConnected();
+
+    ESP_LOGI(TAG, "Make interpreter");
+    static tflite::MicroInterpreter static_interpreter(
+            model, resolver, tensor_arena, kTensorArenaSize);
+    interpreter = &static_interpreter;
+
+    ESP_LOGI(TAG, "Allocate tensors");
+    TfLiteStatus allocate_status = interpreter->AllocateTensors();
+
+    ESP_LOGI(TAG, "Set input");
+    input = interpreter->input(0);
+
+    ESP_LOGI(TAG, "Set output");
+    output = interpreter->output(0);
+
+    ESP_LOGI(TAG, "Set inference_count");
+    inference_count = 0;
 }
 
-void printTest() {
-    ESP_LOGI(TAG, "Hello");
+void inference() {
+    ESP_LOGI(TAG, "Inference");
+
+    float position = static_cast<float>(inference_count) /
+                    static_cast<float>(kInferencesPerCycle);
+    float x = position * kXrange;
+
+    // Quantize the input from floating-point to integer
+    int8_t x_quantized = x / input->params.scale + input->params.zero_point;
+    // Place the quantized input in the model's input tensor
+    input->data.int8[0] = x_quantized;
+
+    // Run inference, and report any error
+    TfLiteStatus invoke_status = interpreter->Invoke();
+    if (invoke_status != kTfLiteOk) {
+        MicroPrintf("Invoke failed on x: %f\n",
+                                static_cast<double>(x));
+        return;
+    }
+
+    // Obtain the quantized output from model's output tensor
+    int8_t y_quantized = output->data.int8[0];
+    // Dequantize the output from integer to floating-point
+    float y = (y_quantized - output->params.zero_point) * output->params.scale;
+
+    // Output the results. A custom HandleOutput function can be implemented
+    // for each supported hardware target.
+    ESP_LOGI(TAG, "%f %f", static_cast<double>(x), static_cast<double>(y));
+
+    // Increment the inference_counter, and reset it if we have reached
+    // the total number per cycle
+    inference_count += 1;
+    if (inference_count >= kInferencesPerCycle) inference_count = 0;
+
+    return;
 }
--- device/ProjectMAIL/main/ondevice_ai.h
+++ device/ProjectMAIL/main/ondevice_ai.h
@@ -3,7 +3,10 @@
 #include "tensorflow/lite/micro/system_setup.h"
 #include "tensorflow/lite/schema/schema_generated.h"
 
-#include <esp_log.h>
+#include <esp_spiffs.h>
 
-// std::unique_ptr<tflite::FlatBufferModel> loadModelFromSPIFFS(const char* model_path);
-void printTest();
+#include <esp_log.h>
+#include <esp_err.h>
+
+void setModel();
+void inference();
