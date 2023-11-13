a9f765eb8ec7d479cac04e01978376f34922dd4b
enduser0@naver.com <enduser0@naver.com>
2023-11-13 14:46:28 +0900

    [S09P31A305-404] ♻️ : Remove unused code

--- device/ProjectMAIL/main/get_set_time.c
+++ device/ProjectMAIL/main/get_set_time.c
@@ -16,19 +16,6 @@ static const char* TAG = "GET_SET_TIME";
 
 void set_time(void)
 {
-    // wifi
-    // esp_err_t ret = nvs_flash_init();
-    // if (ret == ESP_ERR_NVS_NO_FREE_PAGES || ret == ESP_ERR_NVS_NEW_VERSION_FOUND) {
-    //   ESP_ERROR_CHECK(nvs_flash_erase());
-    //   ret = nvs_flash_init();
-    // }
-    // ESP_ERROR_CHECK(ret);
-
-    // ESP_ERROR_CHECK(esp_netif_init());
-    // ESP_ERROR_CHECK(esp_event_loop_create_default());
-
-    // ESP_ERROR_CHECK(example_connect());
-    // ESP_LOGI(TAG, "Connected to AP, begin http example");
 
     // FAST Sever time 동기화
     int server_sec = 0;
